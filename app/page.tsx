f6283ccca415e7e19763b8807ba47ad1cbf74e20
unis5td <lisa1031@naver.com>
2025-07-04 01:32:33 +0900

    feat: crossmint 결제 UI 팝업 모드 및 결제 테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CrossmintProvider, CrossmintEmbeddedCheckout } from "@crossmint/client-sdk-react-ui";
+import { CrossmintProvider, CrossmintHostedCheckout } from "@crossmint/client-sdk-react-ui";
 
 export default function Home() {
     const clientApiKey = process.env.NEXT_PUBLIC_CLIENT_API_KEY as string;
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-start h-screen p-6 bg-white">
             <CrossmintProvider apiKey={clientApiKey}>
                 <div className="max-w-[450px] w-full">
-                    <CrossmintEmbeddedCheckout
+                    <CrossmintHostedCheckout
                         lineItems={{
                             collectionLocator: `crossmint:${collectionId}`,
                             callData: {
@@ -23,15 +23,6 @@ export default function Home() {
                         payment={{
                             crypto: { enabled: true },
                             fiat: { enabled: true },
-                            receiptEmail: currentUserEmail, // ← 로그인한 사용자 이메일 변수
-                        }}
-                        recipient={{
-                        email: currentUserEmail, // ← 로그인한 사용자 이메일 변수
-                        }}
-                         appearance={{
-                            rules: {
-                              ReceiptEmailInput: { display: "hidden" }, // ← 이메일 입력란 숨기기
-                            },
                         }}   
                     />
                 </div>
