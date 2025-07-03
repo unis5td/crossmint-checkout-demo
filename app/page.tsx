a696e178f83c044f240ecf77d00b3da18fb4df15
unis5td <lisa1031@naver.com>
2025-07-04 01:34:46 +0900

    feat: crossmint 결제 UI 팝업 모드 및 결제 테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
     const clientApiKey = process.env.NEXT_PUBLIC_CLIENT_API_KEY as string;
     const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
 
-    const currentUserEmail = "user@example.com"; // 실제로는 로그인 시스템에서 값을 받아와야 함
-
     return (
         <div className="flex flex-col items-center justify-start h-screen p-6 bg-white">
             <CrossmintProvider apiKey={clientApiKey}>
